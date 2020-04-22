730516ee03d7cefd810adf7623bee3d95f5a58cc
Knut Andreas <knut.a.kvale@gmail.com>
2020-04-22 10:58:16 +0200

    Doc setting updates

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..')
+sys.path.insert(0, os.path.abspath('..'))
 matlab_src_dir = os.path.abspath('..')
 matlab_keep_package_prefix = True
 from recommonmark.parser import CommonMarkParser
