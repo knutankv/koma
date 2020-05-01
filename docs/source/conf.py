f4be97d2680d93e15689978a46d41771ea772ae0
Knut Andreas <knut.a.kvale@gmail.com>
2020-05-01 18:34:45 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,13 +54,12 @@ matlab_keep_package_prefix = False
 # The master toctree document.
 master_doc = 'index'
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 nbsphinx_prolog = r"""
 .. raw:: html
@@ -73,4 +72,4 @@ nbsphinx_prolog = r"""
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
