1449785517f67ac75e471ee0e9400cb74bd57914
Knut Andreas <knut.a.kvale@gmail.com>
2020-05-04 09:14:22 +0200

    Documentation config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../'))
 matlab_src_dir = os.path.abspath('../../')
 matlab_keep_package_prefix = True
 from recommonmark.parser import CommonMarkParser
@@ -22,6 +23,8 @@ from recommonmark.transform import AutoStructify
 source_parsers = {
     '.md': CommonMarkParser,
 }
+
+autodoc_mock_imports = ['plotly']
 
 # -- Project information -----------------------------------------------------
 
