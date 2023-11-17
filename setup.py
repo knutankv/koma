89e540e44c2e898ae1b7ea9fb778dae7eafefbec
Knut Andreas Kvåle <knutankv@users.noreply.github.com>
2023-11-17 20:11:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/knutankv/koma",
     packages=setuptools.find_packages(),
-    install_requires=['plotly', 'pandas', 'pyperclip', 'hdbscan'],
+    install_requires=['plotly', 'pandas', 'numpy', 'hdbscan'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
